25ecf1bf79c48fe2be2bc61ed537b651a2146802
JK <u.sslooky@gmail.com>
2021-03-22 04:19:23 +0000

    feat(entrypoint.sh): start desktop in entrypoint. Starts the LXDE desktop in the entrypoint script.
    Signed-off-by: JK <u.sslooky@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,16 @@ Xvfb :0 -screen 0 1024x768x24 &
 export DISPLAY=:0
 
 # Run the VNC Server
-x11vnc -forever -rfbauth ~/.vnc/passwd &
+x11vnc -forever -ncache 10 -rfbauth ~/.vnc/passwd &
 
 # Run noVNC
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 6080 0.0.0.0:5900 &
+
+# Desktop
+export USER="root"
+export HOME="/root"
+/usr/bin/lxpanel --profile LXDE &
+/usr/bin/pcmanfm --desktop --profile LXDE &
 
 # Expose
 yarn global add localtunnel
